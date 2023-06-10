5be96c21bb7202b571fadaf33b2d39f116e8df04
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-06-10 11:46:21 +0300

    added millis to stopwatch
    -> fixed problem where each time the stopwatch update, all timers gets updated even when unchanged.
    
    finished it on the way

--- widget-stopwatch/src/index.js
+++ widget-stopwatch/src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 // setup initial values for stopwatch
-let [seconds, minutes, hours] = [0, 0, 0];
+let [millis, seconds, minutes, hours] = [0, 0, 0];
 
 // time marche
 let marching;
@@ -9,20 +9,12 @@ let isMarching = false;
 function start() {
   if (!isMarching) {
     isMarching = true;
-    
-    marching = setInterval(() => {
-      seconds++;
-      if (seconds % 60 === 0) {
-        seconds = 0;
-        minutes++;
-      }
-      if (minutes % 60 === 0 && seconds % 60 === 0) {
-        minutes = 0;
-        hours++;
-      }
 
-      updateTime(seconds, minutes, hours);
-    }, 1000);
+    marching = setInterval(() => {
+      _incrementTime();
+
+      updateTime();
+    }, 50);
   }
 }
 
@@ -33,16 +25,53 @@ function stop() {
 
 function reset() {
   stop();
-  [seconds, minutes, hours] = [0, 0, 0];
-  updateTime(0, 0, 0);
+  [millis, seconds, minutes, hours] = [0, 0, 0, 0];
+  updateTime(0, 0, 0, 0);
 }
 
-function updateTime(seconds, minutes, hours) {
-  $(".second").html(formatNumber(seconds));
-  $(".minute").html(formatNumber(minutes));
-  $(".hour").html(formatNumber(hours));
+function updateTime() {
+  $(".millis").html(_formatMillis(millis));
+
+  let oldSec = parseInt($('.second').html());
+  let oldMin = parseInt($('.minute').html());
+  let oldHr = parseInt($('.hour').html());
+  
+  if(_hasChanged(oldSec, seconds))
+    $(".second").html(_formatNumber(seconds));
+
+  if(_hasChanged(oldMin, minutes))
+    $(".minute").html(_formatNumber(minutes));
+  
+  if(_hasChanged(oldHr, hours))
+    $(".hour").html(_formatNumber(hours));
 }
 
-function formatNumber(n) {
+function _incrementTime() {
+  millis += 50;
+  if (millis % 1000 === 0) {
+    millis = 0;
+    seconds++;
+  }
+  if (seconds % 60 === 0 && millis % 1000 === 0) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes % 60 === 0 && seconds % 60 === 0 && millis % 1000 === 0) {
+    minutes = 0;
+    hours++;
+  }
+}
+
+function _hasChanged(oldNum, newNum) {
+  return oldNum !== newNum;
+}
+
+function _formatNumber(n) {
   return n < 10 ? `0${n}` : n;
+}
+
+function _formatMillis(n) {
+  if (n < 10) return "00" + n;
+  else if (n < 100) return "0" + n;
+  else return n;
 }
