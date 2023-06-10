00b95cd28342e8951048a9dc370289dd500f71bf
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-06-10 14:37:50 +0300

    final update
    finished adding features and designing stopwatch application:
    -> added saving records mechanism with local storage
    
    -> added a button to remove those records with confirmation
    
    -> some bug fixes

--- widget-stopwatch/src/index.js
+++ widget-stopwatch/src/index.js
@@ -6,6 +6,9 @@ let [millis, seconds, minutes, hours] = [0, 0, 0];
 let marching;
 let isMarching = false;
 
+loadRecords();
+
+// ----------------------- options -----------------------
 function start() {
   if (!isMarching) {
     isMarching = true;
@@ -29,6 +32,82 @@ function reset() {
   updateTime(0, 0, 0, 0);
 }
 
+
+function saveRecord(){
+  stop();
+  let record_name = promptRecord();
+  if(record_name === null) return;
+  
+  let time = `${$('.hour').html()}:${$('.minute').html()}:${$('.second').html()}:${$('.millis').html()}`;
+
+  $('.stopwatch__records-container').append( htmlRecord(record_name, time) );
+  storeRecord(record_name, time);
+}
+// ----------------------- options end -----------------------
+
+
+
+// ---------------------- record-handler ----------------------
+function loadRecords(){
+  let records = JSON.parse(localStorage.getItem('records'));
+
+  for(let x in records){
+    $('.stopwatch__records-container').append( htmlRecord(x, records[x]) )
+  }
+}
+
+function recordExists(name){
+  let rec = JSON.parse(localStorage.getItem('records'));
+  if(rec === null) return false;
+
+  return rec[name] !== undefined;
+}
+
+function storeRecord(name, time){
+  let record;
+
+  // check if record list already exists
+  if(localStorage.getItem('records') === null){
+    record = { [name]: time }
+  }else{
+    record = JSON.parse(localStorage.getItem('records'));
+    record[name] = time;
+  }
+  // save record in browser storage
+  localStorage.setItem('records', JSON.stringify(record));
+}
+
+function clearRecords(){
+  let response = confirm("Are you sure you want to delete all records?");
+
+  response ? (
+    localStorage.removeItem('records'),
+    $('.stopwatch__records-container').html('')
+  ) : null;
+}
+
+function promptRecord(){
+  let record_name;
+  do{
+    record_name = prompt("enter the record name (under 40 characters)");
+  }while(recordExists(record_name) || record_name?.length > 40 || record_name === '');
+
+  return record_name;
+}
+
+function htmlRecord(name, time){
+  return `
+    <div class="stopwatch__single-record">
+      <h3>${name}</h3>
+      <p>${time}</p>
+    </div>
+  `;
+}
+// ---------------------- record-handler end ----------------------
+
+
+
+// ------------------------- helpers -------------------------
 function updateTime() {
   $(".millis").html(_formatMillis(millis));
 
@@ -45,6 +124,7 @@ function updateTime() {
   if(_hasChanged(oldHr, hours))
     $(".hour").html(_formatNumber(hours));
 }
+
 
 function _incrementTime() {
   millis += 50;
@@ -75,3 +155,4 @@ function _formatMillis(n) {
   else if (n < 100) return "0" + n;
   else return n;
 }
+// ------------------------- helpers end -------------------------
